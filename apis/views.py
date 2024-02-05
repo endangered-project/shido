77f1b8baf7c7c4c23bf043b17b57dd952c41588b
Phawit Pornwattanakul <me@helloyeew.dev>
2024-02-05 12:16:44 +0700

    Cover API error exception with JSON response

--- apis/views.py
+++ apis/views.py
@@ -1,9 +1,16 @@
+import logging
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from apis.serializers import ClassSerializer, PropertyTypeSerializer, InstanceSerializer
 from apps.models import Class, PropertyType, Instance
+
+logger = logging.getLogger(__name__)
+internal_server_error_message = JsonResponse({
+    'message': 'Internal server error'
+}, status=500)
 
 
 @csrf_exempt
@@ -12,19 +19,24 @@ def get_class(request):
     """
     View for return all class in JSON format.
     """
-    class_id = request.GET.get('class', None)
-    if class_id:
-        try:
-            class_instance = Class.objects.get(id=class_id)
-            return JsonResponse(ClassSerializer(class_instance).data)
-        except Class.DoesNotExist:
+    try:
+        class_id = request.GET.get('class', None)
+        if class_id:
+            try:
+                class_instance = Class.objects.get(id=class_id)
+                return JsonResponse(ClassSerializer(class_instance).data)
+            except Class.DoesNotExist:
+                return JsonResponse({
+                    'message': 'Class does not exist'
+                }, status=404)
+        else:
             return JsonResponse({
-                'message': 'Class does not exist'
-            }, status=404)
-    else:
-        return JsonResponse({
-            'class': ClassSerializer(Class.objects.all(), many=True).data
-        })
+                'class': ClassSerializer(Class.objects.all(), many=True).data
+            })
+    except Exception as e:
+        logger.error(f'Error when getting class')
+        logger.exception(e)
+        return internal_server_error_message
 
 
 @csrf_exempt
@@ -33,20 +45,25 @@ def get_property_type_from_class(request):
     """
     View for return all property type of class in JSON format.
     """
-    class_id = request.GET.get('class', None)
-    if class_id is None:
-        return JsonResponse({
-            'message': 'No class ID provided'
-        }, status=400)
     try:
-        class_instance = Class.objects.get(id=class_id)
-    except Class.DoesNotExist:
+        class_id = request.GET.get('class', None)
+        if class_id is None:
+            return JsonResponse({
+                'message': 'No class ID provided'
+            }, status=400)
+        try:
+            class_instance = Class.objects.get(id=class_id)
+        except Class.DoesNotExist:
+            return JsonResponse({
+                'message': 'Class does not exist'
+            }, status=404)
         return JsonResponse({
-            'message': 'Class does not exist'
-        }, status=404)
-    return JsonResponse({
-        'property_type': PropertyTypeSerializer(PropertyType.objects.filter(class_instance=class_instance), many=True).data
-    })
+            'property_type': PropertyTypeSerializer(PropertyType.objects.filter(class_instance=class_instance), many=True).data
+        })
+    except Exception as e:
+        logger.error(f'Error when getting property type from class')
+        logger.exception(e)
+        return internal_server_error_message
 
 
 @csrf_exempt
@@ -55,21 +72,26 @@ def get_instance_from_class(request):
     """
     View for return all instance of class in JSON format.
     """
-    class_id = request.GET.get('class', None)
-    if class_id is None:
-        return JsonResponse({
-            'message': 'No class ID provided'
-        }, status=400)
     try:
-        class_instance = Class.objects.get(id=class_id)
-    except Class.DoesNotExist:
+        class_id = request.GET.get('class', None)
+        if class_id is None:
+            return JsonResponse({
+                'message': 'No class ID provided'
+            }, status=400)
+        try:
+            class_instance = Class.objects.get(id=class_id)
+        except Class.DoesNotExist:
+            return JsonResponse({
+                'message': 'Class does not exist'
+            }, status=404)
         return JsonResponse({
-            'message': 'Class does not exist'
-        }, status=404)
-    return JsonResponse({
-        'class': ClassSerializer(class_instance).data,
-        'instance': InstanceSerializer(Instance.objects.filter(class_instance=class_instance), many=True).data
-    })
+            'class': ClassSerializer(class_instance).data,
+            'instance': InstanceSerializer(Instance.objects.filter(class_instance=class_instance), many=True).data
+        })
+    except Exception as e:
+        logger.error(f'Error when getting instance from class')
+        logger.exception(e)
+        return internal_server_error_message
 
 
 @csrf_exempt
@@ -79,12 +101,17 @@ def get_instance(request, instance_id):
     View for return instance in JSON format.
     """
     try:
-        instance = Instance.objects.get(id=instance_id)
-    except Instance.DoesNotExist:
-        return JsonResponse({
-            'message': 'Instance does not exist'
-        }, status=404)
-    return JsonResponse(InstanceSerializer(instance).data)
+        try:
+            instance = Instance.objects.get(id=instance_id)
+        except Instance.DoesNotExist:
+            return JsonResponse({
+                'message': 'Instance does not exist'
+            }, status=404)
+        return JsonResponse(InstanceSerializer(instance).data)
+    except Exception as e:
+        logger.error(f'Error when getting instance')
+        logger.exception(e)
+        return internal_server_error_message
 
 
 @csrf_exempt
@@ -93,34 +120,39 @@ def get_random_instance_from_class(request):
     """
     View for return random instance of class in JSON format.
     """
-    class_id = request.GET.get('class', None)
-    number_of_instance = request.GET.get('number', 1)
-    if class_id is None:
-        return JsonResponse({
-            'message': 'No class ID provided'
-        }, status=400)
     try:
-        number_of_instance = int(number_of_instance)
-    except ValueError:
-        return JsonResponse({
-            'message': 'Number of instance is not a number'
-        }, status=400)
-    if number_of_instance < 1:
-        return JsonResponse({
-            'message': 'Number of instance must be greater than 0'
-        }, status=400)
-    try:
-        class_instance = Class.objects.get(id=class_id)
-        instance_amount = Instance.objects.filter(class_instance=class_instance).count()
-        if number_of_instance > instance_amount:
+        class_id = request.GET.get('class', None)
+        number_of_instance = request.GET.get('number', 1)
+        if class_id is None:
             return JsonResponse({
-                'message': f'Number of instance must be less than or equal to {instance_amount}'
+                'message': 'No class ID provided'
             }, status=400)
-    except Class.DoesNotExist:
+        try:
+            number_of_instance = int(number_of_instance)
+        except ValueError:
+            return JsonResponse({
+                'message': 'Number of instance is not a number'
+            }, status=400)
+        if number_of_instance < 1:
+            return JsonResponse({
+                'message': 'Number of instance must be greater than 0'
+            }, status=400)
+        try:
+            class_instance = Class.objects.get(id=class_id)
+            instance_amount = Instance.objects.filter(class_instance=class_instance).count()
+            if number_of_instance > instance_amount:
+                return JsonResponse({
+                    'message': f'Number of instance must be less than or equal to {instance_amount}'
+                }, status=400)
+        except Class.DoesNotExist:
+            return JsonResponse({
+                'message': 'Class does not exist'
+            }, status=404)
         return JsonResponse({
-            'message': 'Class does not exist'
-        }, status=404)
-    return JsonResponse({
-        'class': ClassSerializer(class_instance).data,
-        'instance': InstanceSerializer(Instance.objects.filter(class_instance=class_instance).order_by('?')[:number_of_instance], many=True).data
-    })
+            'class': ClassSerializer(class_instance).data,
+            'instance': InstanceSerializer(Instance.objects.filter(class_instance=class_instance).order_by('?')[:number_of_instance], many=True).data
+        })
+    except Exception as e:
+        logger.error(f'Error when getting random instance from class')
+        logger.exception(e)
+        return internal_server_error_message
